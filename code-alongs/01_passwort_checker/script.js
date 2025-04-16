1d919f3c0e89e59fe023196548e692774b8c857d
andrikummer <andri.kummer@icloud.com>
2025-04-16 12:09:42 +0200

    code alongs übungen

--- code-alongs/01_passwort_checker/script.js
+++ code-alongs/01_passwort_checker/script.js
@@ -1,1 +1,15 @@
 console.log('hi script.js')
+
+let password = prompt(' Passwort eingeben');
+let username = prompt('Username eingeben');
+
+checkStringLength(password, 12);
+checkStringLength(username, 7);
+
+function checkStringLength(testString, testLength) {
+    if (testString.length >= testLength) {
+        console.log('Dein Text lang genug');
+    } else {
+        console.log('Bitte verwende einen längeren Text');
+    }
+}
